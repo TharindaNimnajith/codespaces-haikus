07d0fc229df648fceaa390bf3f1c8337edc4c52a
Tharinda Nimnajith Rajapaksha <52395884+TharindaNimnajith@users.noreply.github.com>
2022-12-11 20:10:30 +0000

    init

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 let express = require('express');
 let app = express();
+
 let ejs = require('ejs');
+
 const haikus = require('./haikus.json');
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
